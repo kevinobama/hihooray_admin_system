aabf920bad4ec7557d63bad4e33ba3c3f4f7316b
Kevin Gates <picture6542002@yahoo.com>
2017-12-02 21:12:46 -0600

    Update main-local.php

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,18 +3,11 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.0.111;dbname=hooray_exam',
-            'username' => 'root',
-            'password' => 'db3a88',
+            'dsn' => 'mysql:host=x;dbname=x',
+            'username' => 'x',
+            'password' => 'x',
             'charset' => 'utf8',
-        ],
-        // 'db' => [
-            // 'class' => 'yii\db\Connection',
-            // 'dsn' => 'mysql:host=localhost;dbname=hooray_exam',
-            // 'username' => 'root',
-            // 'password' => '654321',
-            // 'charset' => 'utf8',
-        // ],        
+        ],       
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
